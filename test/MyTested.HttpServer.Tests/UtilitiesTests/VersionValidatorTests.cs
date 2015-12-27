3fb7b731ddb2cfa9818f4bf2d43f3e47a86d5761
apxoht <ivaylo.kenov@gmail.com>
2015-12-27 13:53:05 +0200

    Added unit-tests for utilities classes

---
+++ test/MyTested.HttpServer.Tests/UtilitiesTests/VersionValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿namespace MyTested.HttpServer.Tests.UtilitiesTests
+{
+    using System;
+    using Setups;
+    using Utilities;
+    using Xunit;
+
+    public class VersionValidatorTests
+    {
+        [Fact]
+        public void TryParseShouldReturnCorrectVersion()
+        {
+            var version = VersionValidator.TryParse("1.1", TestObjectFactory.GetFailingValidationAction());
+
+            Assert.Equal(1, version.Major);
+            Assert.Equal(1, version.Minor);
+        }
+
+        [Fact]
+        public void TryParseShouldInvokeFailedActionIfStringIsNotInCorrectFormat()
+        {
+            var exception = Assert.Throws<NullReferenceException>(() =>
+            {
+                VersionValidator.TryParse("test", TestObjectFactory.GetFailingValidationAction());
+            });
+
+            Assert.Equal("version valid version string invalid one", exception.Message);
+        }
+    }
+}
