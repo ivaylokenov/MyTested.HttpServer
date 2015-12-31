3065be328728869aa64a97383ceffa59c9912a7e
apxoht <ivaylo.kenov@gmail.com>
2015-12-31 15:50:50 +0200

    Added samples

---
+++ samples/src/MyTested.HttpServer.Samples/AngleSharpTests.cs
@@ -0,0 +1,58 @@
+﻿namespace MyTested.HttpServer.Samples
+{
+    using AngleSharp.Parser.Html;
+    using MyTested.HttpServer;
+    using System.Linq;
+    using System.Net.Http;
+    using Xunit;
+
+    public class AngleSharpTests
+    {
+        private const string BaseAddress = "http://mytestedasp.net";
+
+        IServerBuilder httpServer;
+
+        public AngleSharpTests()
+        {
+            httpServer = MyHttpServer.WorkingRemotely(BaseAddress);
+        }
+
+        [Fact]
+        public void HtmlShouldContainMyTestedHttpServerTitle()
+        {
+            httpServer
+                .WithHttpRequestMessage(req => req.WithMethod(HttpMethod.Get))
+                .ShouldReturnHttpResponseMessage()
+                .WithContent(html =>
+                {
+                    var parser = new HtmlParser();
+                    var document = parser.Parse(html);
+
+                    var element = document
+                        .QuerySelectorAll("h4.content-title")
+                        .FirstOrDefault(el => el.InnerHtml.Contains("My Tested Http Server"));
+
+                    Assert.NotNull(element);
+                });
+        }
+
+        [Fact]
+        public void HtmlShouldContainContactForm()
+        {
+            httpServer
+                .WithHttpRequestMessage(req => req.WithMethod(HttpMethod.Get))
+                .ShouldReturnHttpResponseMessage()
+                .WithContent(html =>
+                {
+                    var parser = new HtmlParser();
+                    var document = parser.Parse(html);
+
+                    var element = document
+                        .QuerySelectorAll("form#contact")
+                        .FirstOrDefault();
+
+                    Assert.NotNull(element);
+                });
+        }
+    }
+}
---
+++ samples/src/MyTested.HttpServer.Samples/JsonTests.cs
@@ -0,0 +1,40 @@
+﻿namespace MyTested.HttpServer.Samples
+{
+    using MyTested.HttpServer;
+    using Newtonsoft.Json.Linq;
+    using System.Linq;
+    using System.Net.Http;
+    using Xunit;
+
+    public class JsonTests
+    {
+        private const string BaseAddress = "https://api.github.com";
+
+        IServerBuilder httpServer;
+
+        public JsonTests()
+        {
+            httpServer = MyHttpServer.WorkingRemotely(BaseAddress);
+        }
+
+        [Fact]
+        public void JsonShouldContainCorrectRepository()
+        {
+            httpServer
+                .WithHttpRequestMessage(req => req
+                    .WithMethod(HttpMethod.Get)
+                    .WithRequestUri("/users/ivaylokenov/repos")
+                    .WithHeader(HttpHeader.UserAgent, "MyTested.HttpServer"))
+                .ShouldReturnHttpResponseMessage()
+                .ContainingContentHeader(HttpContentHeader.ContentType, "application/json; charset=utf-8")
+                .WithContent(content =>
+                {
+                    var repository = JArray.Parse(content)
+                        .Children()
+                        .FirstOrDefault(c => (string)c["name"] == "MyTested.HttpServer");
+
+                    Assert.NotNull(repository);
+                });
+        }
+    }
+}
